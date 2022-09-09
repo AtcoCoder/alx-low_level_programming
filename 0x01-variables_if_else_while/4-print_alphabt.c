b0251340297c4e6427c6a88c67653e55713bd5e8
Omar Jammeh <orms45@gmail.com>
2022-09-09 14:02:12 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,14 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch >= 'z' && ch != 'q' || 'e'; ch++)
+	for (ch = 'a'; ch >= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
 		putchar(ch);
+		}
 	putchar('\n');
+	}
 
 	return (0);
 }
