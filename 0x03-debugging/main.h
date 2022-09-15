5c1981955d0605ee59bdf665a47b04a5c9490048
Omar Jammeh <orms45@gmail.com>
2022-09-15 15:46:39 +0000

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,5 +8,6 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int day, int month);
 void print_remaining_days(int month, int day, int year);
+void print_times_table(int n);
 
 #endif
