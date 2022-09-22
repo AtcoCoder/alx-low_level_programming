224c90cba0e4e341b2ffeb051ad64303eb0273c2
Omar Jammeh <orms45@gmail.com>
2022-09-22 17:38:51 +0000

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,18 +12,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len, res;
+	int len;
 
 	for (len = 0; s1[len] != '\0' && s2[len] != '\0'; len++)
 	{
-		if (s1[len] == s2[len])
+		if (s1[len] != s2[len])
 		{
-			res = 0;
-		}
-		else
-		{
-			res = s1[len] - s2[len];
+			return (s1[len] - s2[len]);
 		}
 	}
-	return (res);
+	return (0);
 }
