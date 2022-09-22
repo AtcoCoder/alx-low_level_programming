9cd36acb11756bec5f4538aa4b73d0ffbe682c77
Omar Jammeh <orms45@gmail.com>
2022-09-22 19:31:54 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int len;
+	int i;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i >= 97 && i <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
