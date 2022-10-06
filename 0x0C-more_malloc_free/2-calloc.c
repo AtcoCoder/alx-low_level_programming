f545e5c361e9ba28bef0ad9a8f8a4d92d850f83f
Omar Jammeh <orms45@gmail.com>
2022-10-06 23:09:56 +0000

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,13 +16,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	array = (void *) malloc(nmemb * sizeof(int));
-	
+
 	if (array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
