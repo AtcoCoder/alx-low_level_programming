7f1ea1739249154213f86c6e2c51369233464aeb
Omar Jammeh <orms45@gmail.com>
2022-10-10 23:49:01 +0000

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+
+/**
+ * struct dog - short description
+ *
+ * @name: pointer to char
+ * @age: float value
+ * @owner: pointer to character
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
