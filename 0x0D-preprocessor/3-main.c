bad8134c0e19ce91b7265be3844480978f369648
Omar Jammeh <orms45@gmail.com>
2022-10-10 17:29:26 +0000

    Task 3

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,22 @@
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+#include <stdio.h>
+
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	j = ABS(-98);
+	i = ABS(98);
+
+	printf("%d %d\n", i, j);
+
+	return (0);
+}
