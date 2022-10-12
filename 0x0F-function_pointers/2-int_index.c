782780c4e36ec33cd09ff225d6d0b6b074baac79
Omar Jammeh <orms45@gmail.com>
2022-10-12 22:16:49 +0000

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array to be searched
+ * @size: size of the array
+ * @cmp: pointer to the search function
+ *
+ * Return: index of the array at which the number is found,
+ * -1, if no match is found, -1 if size less than equal to zero
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
