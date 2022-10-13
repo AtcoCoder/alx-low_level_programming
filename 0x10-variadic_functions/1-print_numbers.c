36ecff4a2d083516698db89d4c1cfd3eb026b27f
Omar Jammeh <orms45@gmail.com>
2022-10-13 22:22:34 +0000

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,14 +21,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		exit(0);
 	}
-	
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, const unsigned int);
 		printf("%d", num);
-		if(i != (n - 1))
+
+		if (i != (n - 1))
 		{
 			if (separator != NULL)
 			{
