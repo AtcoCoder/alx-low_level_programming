e9935ca02f964d78944e6f284e75aaef7c430747
Omar Jammeh <orms45@gmail.com>
2022-10-21 02:58:31 +0000

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 
+
+
+/**
+ * _strlen - finds the length of a string.
+ * @s: string
+ *
+ * Return: length of string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+
 /**
  * add_node - adds a new node at the beginning of a list_t
  *
@@ -11,15 +30,17 @@
 list_t *add_node(const list_t **head, const char *str)
 {
 	list_t *ptr;
+	char *s;
+	
+	int len = _strlen(str);
 
 	ptr = malloc(sizeof(list_t));
+	s = malloc(sizeof(char) * len + 1);
 
 	ptr = (list_t *)head;
 
-	ptr->str = str;
+	ptr->s = str;
+	ptr = ptr->new;
 
-	ptr= ptr->str;
-
-	free(ptr);
 	return (ptr);
 }
