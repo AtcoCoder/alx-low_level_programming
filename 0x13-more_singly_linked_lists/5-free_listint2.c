838c88232e44f5c21d1200aa97b43dbf3cbc223f
orms45 <orms45@gmail.com>
2022-10-24 22:08:12 +0000

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: pointer to pointer to the first node of listint_t list
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *tmp;
+
+	ptr = *head;
+
+	while (ptr != NULL)
+	{
+		tmp = ptr->next;
+		free(ptr);
+		ptr = tmp;
+	}
+	ptr = NULL;
+	*head = ptr;
+}
+
