86926e34f1ddd3c8316441b46e0300ae0c1cf253
orms45 <orms45@gmail.com>
2022-10-25 12:33:27 +0000

    Cleanup task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *ptr;
 
-	if (*head == NULL)
+	if (*head == NULL && idx != 0)
 	{
 		return (NULL);
 	}
@@ -27,7 +27,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = NULL;
 
 	ptr = *head;
 	count = 0;
