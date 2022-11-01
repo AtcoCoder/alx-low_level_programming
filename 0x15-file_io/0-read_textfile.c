fcf6961d64115bb34c6d57c5d8286806a502e99c
orms45 <orms45@gmail.com>
2022-11-01 02:46:02 +0000

    Task 0 error fix 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fd, n_read, n_write;
+	char *s;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
+	s = malloc(letters * sizeof(char) + 1);
+	if (!s)
+		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
+	{
+		free(s);
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	}
+	n_read = read(fd, s, letters);
+	if (n_read == -1)
+	{
+		free(s);
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	}
+	n_write = write(STDOUT_FILENO, s, n_read);
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	free(s);
+	if (n_read == n_write)
+		return (n_write);
+	return (0);
 }
