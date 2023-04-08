40f927c60f52a13e1b37e97a4db78be102f6662a
Omar Jammeh <orms45@gmail.com>
2023-04-08 03:53:12 +0000

    Print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ *
+ * @ht: the hash_table to be printed
+ *
+ * Return: Nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, count, counter = 0;
+	hash_node_t **array;
+	hash_node_t *temp;
+	count = count_keys(ht);
+
+	array = ht->array;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = array[i];
+		while (temp != NULL)
+		{
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			counter++;
+			if (counter != count)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
+
+unsigned long int count_keys(const hash_table_t *ht)
+{
+	unsigned long int i, count = 0;
+	hash_node_t *temp;
+
+	hash_node_t **array = ht->array;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = array[i];
+		while (temp != NULL)
+		{
+			count++;
+			temp = temp->next;
+		}
+	}
+	return (count);
+}
