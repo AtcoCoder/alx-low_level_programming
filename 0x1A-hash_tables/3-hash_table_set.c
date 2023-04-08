15f1ac360b4ffab41daec9d450a6a12cc52f5222
Omar Jammeh <orms45@gmail.com>
2023-04-08 01:33:06 +0000

    Add element update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current_node;
 
-	if ((key == NULL) || strcmp(key, "") != 0)
+	if ((key == NULL) || *key == '\0')
 		return (0);
 	if ((ht == NULL) || (value == NULL))
 		return (0);
