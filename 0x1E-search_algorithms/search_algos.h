c1fd6510b885cfa405f862485b59264d1ff12f03
Omar Jammeh <orms45@gmail.com>
2023-10-14 23:47:56 +0000

    Linear search in skip list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -57,6 +57,6 @@ int _binary_search(int *array, int value, size_t l_bound, size_t u_bound);
 int recursive_binary(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist *list, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
